33e32fd1198ab3d6c3b80f3d2a1e7250bd1f4b2d
mathfuru <mathfuru@gmail.com>
2012-07-20 13:36:42 +0900

    Timeractivityの追加

--- gen/com/example/tmr/R.java
+++ gen/com/example/tmr/R.java
@@ -24,10 +24,14 @@ public final class R {
         public static final int checkBox1=0x7f080000;
         public static final int checkBox2=0x7f080001;
         public static final int checkBox3=0x7f080002;
-        public static final int menu_settings=0x7f080004;
+        public static final int menu_settings=0x7f080007;
+        public static final int radioButton1=0x7f080004;
+        public static final int radioButton2=0x7f080005;
+        public static final int radioButton3=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_timer=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -36,6 +40,9 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int timer_label1=0x7f050004;
+        public static final int timer_label2=0x7f050005;
+        public static final int timer_label3=0x7f050006;
         public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
